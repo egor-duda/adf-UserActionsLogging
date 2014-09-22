def5b0c826b503939d1a23eea95a7202cab88af0
Egor Duda <egor.duda@gmail.com>
2014-09-22 09:35:04 +0000

    Log all events of type ActionEvent

---
+++ ViewController/src/corpit/test/audit/jsf/view/GlobalActionListener.java
@@ -0,0 +1,33 @@
+package corpit.test.audit.jsf.view;
+
+import javax.faces.component.UIComponent;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+
+import oracle.adf.controller.ControllerContext;
+import oracle.adf.share.logging.ADFLogger;
+
+public class GlobalActionListener implements ActionListener {
+    
+    private static ADFLogger logger = ADFLogger.createADFLogger(GlobalActionListener.class);
+    public GlobalActionListener() {
+        super();
+    }
+
+    @Override
+    public void processAction(ActionEvent actionEvent) throws AbortProcessingException {
+        String compId = "*null*";
+        String viewId = "*null*";
+        UIComponent comp = actionEvent.getComponent();
+        if (comp != null) {
+            compId = comp.getId() + "/" + comp.getClass().getSimpleName();
+        }
+        ControllerContext controllerContext = ControllerContext.getInstance();
+        if (controllerContext != null && 
+            controllerContext.getCurrentViewPort() != null) {
+            viewId = controllerContext.getCurrentViewPort().getViewId();
+        }
+        logger.fine("Action on " + compId + "@" + viewId);
+    }
+}
