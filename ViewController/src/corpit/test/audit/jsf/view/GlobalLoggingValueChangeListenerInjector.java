edf2f53312637b220ea1b6dde05bea89fe7287ba
Egor Duda <egor.duda@gmail.com>
2015-03-16 08:38:25 +0000

    Log all af:dialog and af:serverListener events

--- ViewController/src/corpit/test/audit/jsf/view/GlobalLoggingValueChangeListenerInjector.java
+++ ViewController/src/corpit/test/audit/jsf/view/GlobalLoggingValueChangeListenerInjector.java
@@ -1,5 +1,8 @@
 package corpit.test.audit.jsf.view;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import java.util.Iterator;
 
 import javax.faces.component.EditableValueHolder;
@@ -12,11 +15,15 @@ import javax.faces.event.SystemEventListener;
 import javax.faces.event.ValueChangeListener;
 
 import oracle.adf.share.logging.ADFLogger;
+import oracle.adf.view.rich.component.rich.RichDialog;
+import oracle.adf.view.rich.event.ClientListenerSet;
+import oracle.adf.view.rich.event.DialogListener;
 
 public class GlobalLoggingValueChangeListenerInjector implements SystemEventListener {
     
     private static ADFLogger logger = ADFLogger.createADFLogger(GlobalLoggingValueChangeListenerInjector.class);
     static private LoggingValueChangeListener globalLoggingValueChangeListener = new LoggingValueChangeListener ();
+    static private LoggingDialogListener globalLoggingDialogListener = new LoggingDialogListener ();
     
     public GlobalLoggingValueChangeListenerInjector() {
         super();
@@ -46,9 +53,60 @@ public class GlobalLoggingValueChangeListenerInjector implements SystemEventList
         return;
     }
 
+    private void recursiveAddGlobalDialogListener (UIComponent comp) {
+        if (comp == null) return;
+        if (comp instanceof RichDialog) {
+            RichDialog dialogComponent = (RichDialog)comp;
+            boolean alreadyInjected = false;
+            for (DialogListener dialogListener: dialogComponent.getDialogListeners()) {
+                if (dialogListener instanceof LoggingDialogListener) {
+                    alreadyInjected = true;
+                    break;
+                }
+            }
+            if (!alreadyInjected) {
+                dialogComponent.addDialogListener (globalLoggingDialogListener);
+                logger.finest ("attached valueChangeListener to " + comp.getId());
+            }
+            return;
+        }
+        Iterator<UIComponent> children = comp.getFacetsAndChildren();
+        while (children.hasNext()) {
+            recursiveAddGlobalDialogListener(children.next());
+        }
+        return;
+    }
+    
+    private void recursiveAddGlobalClientListener (UIComponent comp) {
+        if (comp == null) return;
+        try {
+            Method getClientListenersMethod = comp.getClass().getMethod("getClientListeners");
+            Method setClientListenersMethod = comp.getClass().getMethod("setClientListeners", ClientListenerSet.class);
+            ClientListenerSet originalClientListenerSet = (ClientListenerSet) getClientListenersMethod.invoke (comp);
+            if (originalClientListenerSet != null) {
+                LoggingClientListenerSet newClientListenerSet = new LoggingClientListenerSet (originalClientListenerSet);
+                setClientListenersMethod.invoke(comp, newClientListenerSet);
+            }
+        } catch (NoSuchMethodException e1) {
+            ; // no client listeners mechanism for this component, skip
+        } catch (InvocationTargetException e2) {
+            logger.severe ("InvocationTargetException trying to modify client listeners for class " + comp.getClass().getName() , e2);
+        } catch (IllegalAccessException e3) {
+            logger.severe ("IllegalAccessException trying to modify client listeners for class " + comp.getClass().getName() , e3);                
+        }
+        Iterator<UIComponent> children = comp.getFacetsAndChildren();
+        while (children.hasNext()) {
+            recursiveAddGlobalClientListener(children.next());
+        }
+        return;
+    }
+
+
     @Override
     public void processEvent(SystemEvent systemEvent) throws AbortProcessingException {
         recursiveAddGlobalValueChangeListener ((UIViewRoot)systemEvent.getSource());
+        recursiveAddGlobalClientListener ((UIViewRoot)systemEvent.getSource());
+        recursiveAddGlobalDialogListener ((UIViewRoot)systemEvent.getSource());
     }
 
     @Override
--- ViewController/src/corpit/test/audit/jsf/view/MainBean.java
+++ ViewController/src/corpit/test/audit/jsf/view/MainBean.java
@@ -1,17 +1,43 @@
 package corpit.test.audit.jsf.view;
 
+import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 
 import oracle.adf.share.logging.ADFLogger;
+import oracle.adf.view.rich.component.rich.RichPopup;
+import oracle.adf.view.rich.event.DialogEvent;
+import oracle.adf.view.rich.render.ClientEvent;
 
 public class MainBean {
     
     private static ADFLogger logger = ADFLogger.createADFLogger(MainBean.class);
-    
+    private RichPopup dialogPopupComponent;
+
     public MainBean() {
     }
 
     public void handleDepartmentNameChange(ValueChangeEvent valueChangeEvent) {
         logger.fine ("Department name changed to " + valueChangeEvent.getNewValue());
+    }
+    
+    public void onLoad (ClientEvent clientEvent) {
+        clientEvent.hashCode();
+    }
+
+    public void callDialog (ActionEvent actionEvent) {
+        actionEvent.hashCode();
+        dialogPopupComponent.show(new RichPopup.PopupHints());
+    }
+
+    public void setDialogPopupComponent (RichPopup dialogPopupComponent) {
+        this.dialogPopupComponent = dialogPopupComponent;
+    }
+
+    public RichPopup getDialogPopupComponent () {
+        return dialogPopupComponent;
+    }
+
+    public void dialogListener (DialogEvent dialogEvent) {
+        logger.info (dialogEvent.getOutcome().toString());
     }
 }
